a55839df9e0d45a16704bf363e22b9a799855039
Haohan Lin <haohan.lin@outlook.com>
2021-05-30 12:34:20 -0700

    add cs61b skeleton code

---
+++ java_practice/Dog.java
@@ -0,0 +1,10 @@
+public class Dog{
+    /**dog barks */
+    public void bark(){
+      System.out.println("Moo");
+    }
+    /**dog runs fast! */
+    public static void runFast(){
+      System.out.println("Ruff Run");
+    }
+  }
---
+++ java_practice/TestDog.java
@@ -0,0 +1,7 @@
+public class TestDog {
+    public static void main(String[] args) {
+        Dog poppa = new Dog();
+        poppa.bark();
+        Dog.runFast();
+    }
+}
