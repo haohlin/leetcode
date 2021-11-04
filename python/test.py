772817bc6787b7848ebfd453ec620ee642457a70
Haohan Lin <haohan.lin@outlook.com>
2021-11-04 09:41:16 -0700

    upload

--- python/test.py
+++ python/test.py
@@ -1,41 +1,15 @@
-# 两数相除
+input_n = 1
+n = 0
+current_mult = 1
+remain = input_n
 
-# 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
-
-# 返回被除数 dividend 除以除数 divisor 得到的商。
-
-# 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
-
-import math
-
-
-
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
-        if divisor == 0:
-            return math.nan
-        divisor_abs = abs(divisor)
-        dividend_abs = abs(dividend)
-        sum_div = 0
-        count = 0
-        while True:
-            if dividend_abs - sum_div < divisor_abs:
-                break
-            sum_div += divisor_abs
-            count += 1
-                
-        
-        if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
-            return -count
-        else:
-            return count
+while remain > 0:
+    while current_mult << 1 <= remain:
+        current_mult <<= 1
+    
+    n += 1
+    remain = remain - current_mult
+    current_mult = 1
 
 
-sol = Solution()
-result1 = sol.div(-10,0)
-# result2 = sol.div(-5,-2)
-# result3 = sol.div(5,-2)
-
-print(result1)
-# print(result2)
-# print(result3)
+print(n)
